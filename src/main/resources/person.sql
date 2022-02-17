c0bdcfd31c25705e925016e3842d17e7afd7f030
Haze-S <skdml20022@gmail.com>
2022-02-18 04:16:29 +0900

    spring-batch

---
+++ src/main/resources/person.sql
@@ -0,0 +1,13 @@
+-- create table person (
+--     id bigint primary key auto_increment,
+--     name varchar(255),
+--     age varchar(255),
+--     address varchar(255)
+-- );
+
+insert into person(name, age, address)
+values('챠니', '20', '인천');
+insert into person(name, age, address)
+values('정현', '25', '서울');
+insert into person(name, age, address)
+values('안개', '30', '부산');
